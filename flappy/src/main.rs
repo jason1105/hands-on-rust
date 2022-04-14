7f2bd8392490f50fde5dd93196e3446efe19c1ea
lv-wei <13366824@qq.com>
2022-04-14 18:50:36 +0800

    Add game mode to flappy

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -1,68 +1,71 @@
 use bracket_lib::prelude::*;
 
 struct State {
-    component: Option<Bounce>,
+    mode: GameMode, 
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        let (x, y) = self.change_position(ctx);
-        ctx.print(x, y, "hello world.");
-    }
-}
-
-impl State {
-    fn change_position(&mut self, ctx: &BTerm) -> (u32, u32) {
-        match &mut self.component {
-            Some(bounce) => {
-                match bounce.x_dir {
-                    true => bounce.x += 2,
-                    false => {
-                        bounce.x = bounce.x.saturating_sub(2);
-                    }
-                }
-                match bounce.y_dir {
-                    true => bounce.y += 1,
-                    false => {
-                        bounce.y = bounce.y.saturating_sub(1);
-                    }
-                }
-
-                match bounce.x {
-                    80 => bounce.x_dir = false,
-                    0 => bounce.x_dir = true,
-                    _ => (),
-                }
-                match bounce.y {
-                    50 => bounce.y_dir = false,
-                    0 => bounce.y_dir = true,
-                    _ => (),
-                }
-
-                (bounce.x, bounce.y)
-            }
-            None => {
-                let _ = self.component.insert(Bounce {
-                    x: 0,
-                    x_dir: true,
-                    y: 0,
-                    y_dir: true,
-                });
-                (0, 0)
-            }
+        match self.mode {
+            GameMode::Menu => (self.main_menu(ctx)),
+            GameMode::Playing => (self.play(ctx)),
+            GameMode::End => (self.dead(ctx)),
         }
     }
 }
 
-struct Bounce {
-    x: u32,
-    x_dir: bool,
-    y: u32,
-    y_dir: bool,
+impl State {
+    fn new() -> State {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        // TODO 
+        ctx.cls();
+        ctx.print_centered(5, "Welcom to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(10, "(Q) Quit Game");
+        match ctx.key {
+           Some(VirtualKeyCode::P) => self.mode = GameMode::Playing,
+           Some(VirtualKeyCode::Q) => ctx.quitting = true,
+           _ => ()
+
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        // TODO
+       self.mode = GameMode::End; 
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        // TODO
+        ctx.cls();
+        ctx.print_centered(5, "You are dead.");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(10, "(Q) Quit Game");
+        match ctx.key {
+           Some(VirtualKeyCode::P) => self.mode = GameMode::Playing,
+           Some(VirtualKeyCode::Q) => ctx.quitting = true,
+           _ => ()
+
+        }
+    }
+
+    fn restart(&mut self, ctx: &mut BTerm) {
+         // TODO
+         self.mode = GameMode::Playing;
+    }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50().with_title("flappy").build()?;
-    main_loop(context, State { component: None })
+    main_loop(context, State::new())
 }
