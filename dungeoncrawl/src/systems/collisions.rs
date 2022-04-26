1bbdac9644f4190887762858f4e9fea9d6a9f2ad
lv-wei <13366824@qq.com>
2022-04-26 16:47:42 +0800

    adding collision system

---
+++ dungeoncrawl/src/systems/collisions.rs
@@ -0,0 +1,22 @@
+use crate::prelude::*;
+
+#[system]
+#[read_component(Enemy)]
+#[read_component(Player)]
+#[read_component(Point)]
+pub fn collision(ecs: &SubWorld, commands: &mut CommandBuffer) {
+    let mut player_pos = Point::zero();
+
+    <&Point>::query()
+        .filter(component::<Player>())
+        .iter(ecs)
+        .for_each(|point| {
+            player_pos = *point;
+        });
+
+    <(Entity, &Point)>::query()
+        .filter(component::<Enemy>())
+        .iter(ecs)
+        .filter(|(_, pos)| **pos == player_pos)
+        .for_each(|(entity, _)| commands.remove(*entity));
+}
--- dungeoncrawl/src/systems/mod.rs
+++ dungeoncrawl/src/systems/mod.rs
@@ -1,12 +1,13 @@
 use crate::prelude::*;
 
+use self::collisions::collision_system;
 use self::entity_render::entity_render_system;
 use self::map_render::map_render_system;
 use self::player_input::player_input_system;
+mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
-
 /// Build systems
 
 pub fn build_scheduler() -> Schedule {
@@ -14,5 +15,6 @@ pub fn build_scheduler() -> Schedule {
         .add_system(player_input_system())
         .add_system(map_render_system())
         .add_system(entity_render_system())
+        .add_system(collision_system())
         .build()
 }
