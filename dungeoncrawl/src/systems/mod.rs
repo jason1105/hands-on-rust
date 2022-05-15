034806b0c077783a1117d6b5e63dfbd35c17a594
lv-wei <13366824@qq.com>
2022-05-15 16:54:59 +0800

    Update comment

--- dungeoncrawl/src/systems/mod.rs
+++ dungeoncrawl/src/systems/mod.rs
@@ -20,45 +20,64 @@ mod player_input;
 mod random_move;
 mod tooltips;
 
-/// Build systems
+///! Build systems
 
+/// handle input event
 pub fn build_input_schedule() -> Schedule {
     Schedule::builder()
+        // hud
         .add_system(hud_system())
+        // handle input, create player movement message
         .add_system(player_input_system())
         .flush()
+        // render screen
         .add_system(map_render_system())
         .add_system(entity_render_system())
         .add_system(tooltips_system())
         .build()
 }
 
+/// handle player
 pub fn build_player_schedule() -> Schedule {
     Schedule::builder()
+        // hub
         .add_system(hud_system())
+        // process player's movement
         .add_system(movement_system())
         .flush() // flush command buffer
+        // identify collision
         .add_system(collision_system())
         .flush()
+        // render screen
         .add_system(map_render_system())
         .add_system(entity_render_system())
-        .add_system(end_turn_system())
+        // show tooltips
         .add_system(tooltips_system())
+        // move to next state
+        .add_system(end_turn_system())
         .build()
 }
 
+/// handle monsters
 pub fn build_monster_schedule() -> Schedule {
     Schedule::builder()
+        // hud
         .add_system(hud_system())
+        // monsters do move
         .add_system(random_move_system())
         .flush()
+        // process monster's movement
         .add_system(movement_system())
         .flush() // flush command buffer
+        // process collision
         .add_system(collision_system())
         .flush()
+        // render screen
         .add_system(map_render_system())
         .add_system(entity_render_system())
-        .add_system(end_turn_system())
+        // show tooltips
         .add_system(tooltips_system())
+        // move to first state
+        .add_system(end_turn_system())
         .build()
 }
